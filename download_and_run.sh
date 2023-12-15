53e1c56305e2076935c13260f1ccf6863319f552
Will Delabretonne <willdelabretonne@Wills-MacBook-Pro.local>
2023-12-15 13:49:29 -0600

    testing

--- download_and_run.sh
+++ download_and_run.sh
@@ -30,5 +30,5 @@ python3 /app/main.py $SCRIPT_PATH
 # Pushing results to cyverse
 RESULTS_PATH="$SCRIPT_PATH/Output"
 
-iput -K -f -b -r -T --retries 5 -X output_checkpoint_file $RESULTS_PATH $IRODS_PATH
+iput -K -f -b -r -T --retries 5 -X output_checkpoint_file $RESULTS_PATH/* $IRODS_PATH
 
