c220e37f3b32e131da1224b7a11eb29b8381ae67
Will Delabretonne <ldelaB2@outlook.com>
2024-02-05 10:43:48 -0600

    docker

--- docker/download_and_run.sh
+++ docker/download_and_run.sh
@@ -20,15 +20,27 @@ cd /app/working_directory
 # Downloading user specified params
 iget -K -r -T --retries 5 -X input_checkpoint_file $IRODS_PATH .
 
-# Defining input path
-#LAST_FOLDER=$(basename $IRODS_PATH)
-#SCRIPT_PATH="/app/working_directory/$LAST_FOLDER"
+# Extract the img path
+irdos_img_path=$(jq -r '.input_path' $(basename $IRODS_PATH))
+irods_output_path=$(jq -r '.output_path' $(basename $IRODS_PATH))
+
+# Downloading the image
+iget -K -r -T --retries 5 -X input_checkpoint_file $irods_img_path .
+
+# Update img_path and param file
+local_img_path="$(pwd)/$(basename $img_path)"
+local_output_path="$(pwd)/$(basename $irods_output_path)"
+
+jq '.input_path = "'$local_img_path'" | .output_path = "'$local_output_path'"' $(basename $IRODS_PATH) > temp.json
+mv temp.json $(basename $IRODS_PATH)
+
+# Update the path to the params file
+local_params_path="$(pwd)/$(basename $IRODS_PATH)"
 
 # Running the script
-#python3 /app/scripts/main.py $SCRIPT_PATH
+python3 /app/scripts/main.py $local_params_path
 
 # Pushing results to cyverse
-#cd $SCRIPT_PATH
-#iput -K -f -b -r -T --retries 5 -X output_checkpoint_file Output $IRODS_PATH
+#iput -K -f -b -r -T -f --retries 5 -X output_checkpoint_file  $IRODS_PATH
 
 tail -f /dev/null
